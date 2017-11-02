93e6f371f86daa9b12f935ee9eade50b3a4053a7
Patrik Bogren <mmFooD@users.noreply.github.com>
2017-11-02 19:08:57 +0100

    tests autocomplete as module

--- tests/test_autocomplete.py
+++ tests/test_autocomplete.py
@@ -1,41 +1,36 @@
 import pytest
 
-from fbseries.controller import Controller
+from fbseries.autocomplete import autocomplete
 
 
 class TestAutocomplete():
 
-    @pytest.fixture
-    def app(self):
-        controller = Controller()
-        return controller
-
-    def test_returns_empty_if_no_match(self, app):
+    def test_returns_empty_if_no_match(self):
         name_list = ['arsenal', 'blackpool']
-        result = app.autocomplete('k', name_list)
+        result = autocomplete('k', name_list)
         expected = ""
         assert result == expected
 
-    def test_returns_entire_name_if_only_match(self, app):
+    def test_returns_entire_name_if_only_match(self):
         name_list = ['arsenal', 'blackpool']
-        result = app.autocomplete('a', name_list)
+        result = autocomplete('a', name_list)
         expected = 'arsenal'
         assert result == expected
 
-    def test_returns_least_common__denominator_if_multiple_matches(self, app):
+    def test_returns_least_common__denominator_if_multiple_matches(self):
         name_list = ['blackburn', 'blackpool']
         expected = 'black'
-        result = app.autocomplete('b', name_list)
+        result = autocomplete('b', name_list)
         assert result == expected
 
-    def test_can_take_strings_with_spaces(self, app):
+    def test_can_take_strings_with_spaces(self):
         name_list = ['man utd']
         expected = 'man utd'
-        result = app.autocomplete('m', name_list)
+        result = autocomplete('m', name_list)
         assert result == expected
 
-    def test_keeps_case_formatting(self, app):
+    def test_keeps_case_formatting(self):
         name_list = ['Man Utd']
         expected = 'Man Utd'
-        result = app.autocomplete('m', name_list)
+        result = autocomplete('m', name_list)
         assert result == expected
--- tests/test_table.py
+++ tests/test_table.py
@@ -1,9 +1,3 @@
 from fbseries.controller import Controller
 
 
-def test_returns_empty_if_no_match():
-    controller = Controller()
-    name_list = ['arsenal', 'blackpool']
-    controller
-    result = controller.autocomplete('k', name_list)
-    assert not result
